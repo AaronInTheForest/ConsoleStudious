20910b07aa694891f8b8e47b7c891483d665678d
Aaron Champagne <aaron.m.champagne@gmail.com>
2020-02-19 20:50:17 -0700

    Created Prompt(string) 1. Main method code tests Propt() with HelloWorld. 2. Prompt accepts a string to display to the user. 3. Prompt accepts user input after user hits enter. 4. If response is blank we display an error messge and then re-read the user input.

--- ConsoleStudious/ConsoleStudious/Program.cs
+++ ConsoleStudious/ConsoleStudious/Program.cs
@@ -7,6 +7,28 @@ namespace ConsoleStudious
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var name = Prompt("What is your name?");
+
+            Console.WriteLine($"Hello {name}!");
+
+            Console.ReadKey();
+        }
+        public static string Prompt(string question)
+        {
+            Console.WriteLine(question);
+            Console.WriteLine("(press enter to continue...)");
+            string response;
+            do
+            {
+                response = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(response))
+                {
+                    Console.WriteLine("Response cannot be blank.");
+                }
+            } while (string.IsNullOrWhiteSpace(response));
+            
+            return response;
         }
     }
 }
